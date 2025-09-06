4aa094d3d11a3ffa55789c73b1dc61886bc60ec5
EllisEberwein <168472597+EllisEberwein@users.noreply.github.com>
2025-09-06 18:38:31 -0400

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class HW1 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); // add a scanner to be able to input an integer
+    System.out.println("How old are you? "); // ask their age
+    String age = sc.nextLine(); // receive the input
+    System.out.println("You are " + age + " years old"); // print out results (:
+//can you do some math for me? take these variables and print out the sum of them!
+    int num1 = 10;
+    int num2 = 37;
+    System.out.println("The sum is...");
+    System.out.println(num1 + num2) ;//add the numbers together
+//?
+
+    }
+}
